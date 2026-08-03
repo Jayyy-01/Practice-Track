86a64cb6320bed56451af7c3652651d920a6fda7
Jayasree <pjayasree543@gmail.com>
2026-08-03 19:36:19 +0530

    Prime Number

---
+++ day-01/prime.py
@@ -0,0 +1,11 @@
+number = int(input("enter the number: "))
+if number < 2:
+    print("Not a prime number")
+elif number > 2:
+    for i in range(2,number):
+        if number % i == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+    
